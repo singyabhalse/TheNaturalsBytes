34b0d64cc4738044b1e82f4396f4dd0292843481
singyabhalse <singyabhalse79@gmail.com>
2026-07-13 15:27:07 +0530

    ExceptionHandling files

--- EATS/src/main/java/com/userexprior/exception/ExceptionHandling.java
+++ EATS/src/main/java/com/userexprior/exception/ExceptionHandling.java
@@ -2,9 +2,6 @@ package com.userexprior.exception;
 
 public class ExceptionHandling extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public ExceptionHandling() {
