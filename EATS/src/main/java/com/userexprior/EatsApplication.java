4113652112d04a089faf5b27d2689e2b470d4477
singyabhalse <singyabhalse79@gmail.com>
2026-07-16 21:42:21 +0530

    added code

--- EATS/src/main/java/com/userexprior/EatsApplication.java
+++ EATS/src/main/java/com/userexprior/EatsApplication.java
@@ -3,14 +3,11 @@ package com.userexprior;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import com.userexprior.config.ClassPathScannerConfig;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.userexprior")
 @EnableScheduling
-@Import(ClassPathScannerConfig.class)
 public class EatsApplication {
 
 	public static void main(String[] args) {
