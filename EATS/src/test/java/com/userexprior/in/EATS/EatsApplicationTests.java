71a727aabbd5c23c3b66e3d2782568bd3b7961a3
singyabhalse <singyabhalse79@gmail.com>
2026-07-16 22:02:55 +0530

    added test case

--- EATS/src/test/java/com/userexprior/in/EATS/EatsApplicationTests.java
+++ EATS/src/test/java/com/userexprior/in/EATS/EatsApplicationTests.java
@@ -1,13 +1,15 @@
-//package com.userexprior.in.EATS;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class EatsApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+/*
+package com.userexprior.in.EATS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
