d2222daf0cf97131f6040fbdae5788a936274098
singyabhalse <singyabhalse79@gmail.com>
2026-07-17 19:09:30 +0530

    JWT security implemented.

---
+++ EATS/src/main/java/com/userexprior/security/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.userexprior.security;
+
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET = "mysecretkeymysecretkeymysecretkey"; // use at least 256-bit key
+    private final long EXPIRATION = 1000 * 60 * 1; // 1 hour
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+            System.out.println("Token expired at: " + e.getClaims().getExpiration());
+            return false;
+        } catch (JwtException e) {
+            System.out.println("Invalid token: " + e.getMessage());
+            return false;
+        }
+    }
+
+}
