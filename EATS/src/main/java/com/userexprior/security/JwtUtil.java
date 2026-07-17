50c48a0491d0f1928d4ca0cd71a4274012eae20a
singyabhalse <singyabhalse79@gmail.com>
2026-07-17 20:28:49 +0530

    removed space

--- EATS/src/main/java/com/userexprior/security/JwtUtil.java
+++ EATS/src/main/java/com/userexprior/security/JwtUtil.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-
 import java.security.Key;
 import java.util.Date;
 
